73b7d195c12cd840c0955d43fa965ac8c582a789
iceinto <iceinto@163.com>
2022-12-26 16:46:14 +0800

    add design-pattern

--- design-pattern/src/lib.rs
+++ design-pattern/src/lib.rs
@@ -1,9 +1,16 @@
+
 #[cfg(test)]
 mod tests {
+    use crate::first_word_main;
+
     #[test]
     fn it_works() {
-        println!("ch05-01-defining-structs");
         let result = 2 + 2;
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_first_word_main() {
+        first_word_main()
     }
 }
